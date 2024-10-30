2286513bc900bd08817667bb28b778347e7422d2
jeka_e <jeka_egorova@mail.ru>
2024-10-30 13:30:10 +0100

    fix closing

--- crawler_src/Crawler.py
+++ crawler_src/Crawler.py
@@ -96,7 +96,7 @@ class Crawler:
             close_button = page.locator(
                 "button.modal__close-button, button[role='button']:has-text('Close')"
             )
-            if close_button.count() == 1:
+            if close_button.is_visible():
                 close_button.click()
                 print("Pop-up closed successfully.")
             else:
@@ -112,7 +112,7 @@ class Crawler:
         with sync_playwright() as p:
             #Launch with visual browser
             url_core = url.strip()
-            browser = p.chromium.launch(headless=True)
+            browser = p.chromium.launch(headless=False)
             url_type = "news" if "news" in self.output_path else "gov"
             context = browser.new_context(record_har_path=f"{self.output_path}/{url_core}_{url_type}.har",
                                           record_video_dir=f"{self.output_path}/") # new profile
@@ -122,7 +122,7 @@ class Crawler:
             end_time = time.time()
             page_load_time = end_time - start_time
             print(f"Page load time: {page_load_time}")
-            time.sleep(10)  # TODO this has to be 10 according to the assignment
+            time.sleep(10)
             print(f"Screenshotting {url_core}_before_cookies.png")
             page.screenshot(path=f"{self.output_path}/{url_core}_{url_type}_pre_consent.png", full_page=True)
             self.accept_cookies(page=page)
